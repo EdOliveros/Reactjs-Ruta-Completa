037f5e1a51d65934e34f22490a5acb4d63973086
ed oliveros <edgarolimoy98@gmail.com>
2023-05-10 23:21:39 -0500

    feat: :art: Mejorando aun mas la declaracion del render para Reactjs 17

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root')
+render(<App />, root);
