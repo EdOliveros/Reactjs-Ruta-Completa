dd85dfc7fc8a74bc17de03f2fcc3eafb6a6600d1
ed oliveros <edgarolimoy98@gmail.com>
2023-05-11 15:12:32 -0500

    feat: :sparkles: State sin context, pura composicion de componentes en App!

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,61 @@ import { AppUI } from './AppUI';
 import React from 'react';
 
 function App() {
+
+  const [state, setState] = React.useState('My State')
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state} />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+    </>
   );
 }
+
+function TodoHeader({ children }) {
+  return (
+    <header>
+      { children }
+    </header>
+  );
+}
+
+function TodoList({ children }) {
+  return (
+    <>
+      { children }
+    </>
+  );
+}
+
+function TodoCounter() {
+  return <p>Todo counter</p>;
+}
+
+function TodoSearch() {
+  return <p>Todo Search</p>;
+}
+
+function TodoItem({state}) {
+  return <p>Todo Item: { state }</p>;
+}
+
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
