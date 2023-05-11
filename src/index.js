d0e4d609104dc1db119eee970c4e9878daf5639d
ed oliveros <edgarolimoy98@gmail.com>
2023-05-10 23:15:04 -0500

    feat: :tada: Reto terminado!! Logre pasar mi proyecto de react 18 a react 17 de manera exitosa :D

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
