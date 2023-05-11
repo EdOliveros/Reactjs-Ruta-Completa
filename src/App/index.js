49419f94efefe8b4a5341e310cfecfc51f37457f
ed oliveros <edgarolimoy98@gmail.com>
2023-05-11 15:19:07 -0500

    feat: :art: Ejercisio terminado, regresando al inicio.

--- src/App/index.js
+++ src/App/index.js
@@ -3,61 +3,11 @@ import { AppUI } from './AppUI';
 import React from 'react';
 
 function App() {
-
-  const [state, setState] = React.useState('My State')
-
   return (
-    <>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
-      <TodoList>
-        <TodoItem state={state} />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-      </TodoList>
-    </>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
-
-function TodoHeader({ children }) {
-  return (
-    <header>
-      { children }
-    </header>
-  );
-}
-
-function TodoList({ children }) {
-  return (
-    <>
-      { children }
-    </>
-  );
-}
-
-function TodoCounter() {
-  return <p>Todo counter</p>;
-}
-
-function TodoSearch() {
-  return <p>Todo Search</p>;
-}
-
-function TodoItem({state}) {
-  return <p>Todo Item: { state }</p>;
-}
-
-
-// function App() {
-//   return (
-//     <TodoProvider>
-//       <AppUI />
-//     </TodoProvider>
-//   );
-// }
 
 export default App;
