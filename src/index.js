b7ed4914f2c2e54cd7e0f44ea6ceaaa52014d565
ed oliveros <edgarolimoy98@gmail.com>
2023-05-13 13:44:14 -0500

    Hoc Aprendidos, ahora empezamos a integrarlos

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-
-function App(props) {
-    return (
-        <h1>{props.saludo}, {props.nombre} !!</h1>
-    )
-}
-
-function whitSaludo(WrappedComponent) {
-   return function WrappedComponentWhitSaludo (saludo) {
-    return function ComponenteDeVerdad(props) {
-        return (
-            <>
-                <WrappedComponent  {...props} saludo={saludo}/>
-                <h2>Estamos acompañando al wrapped component</h2>
-            </>
-        )
-    }
-   }
-}
-
-const AppWhitSaludo = whitSaludo(App)('Hola')
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AppWhitSaludo
-        
-        nombre='Ed'
-    />
+    <App />
 );
