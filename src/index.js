c3f484cb22d9219180d4cbe7024c2bae8dbd5a3d
ed oliveros <edgarolimoy98@gmail.com>
2023-05-12 23:41:57 -0500

    feat: :art: HOCs Hight Order Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+function App(props) {
+    return (
+        <h1>{props.saludo}, {props.nombre} !!</h1>
+    )
+}
+
+function whitSaludo(WrappedComponent) {
+   return function WrappedComponentWhitSaludo (saludo) {
+    return function ComponenteDeVerdad(props) {
+        return (
+            <>
+                <WrappedComponent  {...props} saludo={saludo}/>
+                <h2>Estamos acompañando al wrapped component</h2>
+            </>
+        )
+    }
+   }
+}
+
+const AppWhitSaludo = whitSaludo(App)('Hola')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <AppWhitSaludo
+        
+        nombre='Ed'
+    />
+);
