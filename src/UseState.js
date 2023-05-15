15b8f859aae54199264bbe0d3b49c3e7929d8d57
ed oliveros <edgarolimoy98@gmail.com>
2023-05-15 14:27:55 -0500

    feat: :art: Use Effect protegiendo el llamado a un hipotetico backen!

--- src/UseState.js
+++ src/UseState.js
@@ -2,7 +2,24 @@ import React from "react";
 
 function UseState({ name }) {
 
-    const [ error, setError ] = React.useState(false)
+    const [ error, setError ] = React.useState(true)
+    const [ loading, setLoading ] = React.useState(false)
+
+    React.useEffect(() => {
+        console.log('empezando el efecto')
+
+        if(!!loading) {
+            setTimeout(() => {
+                console.log('haciendo la validacion')
+    
+                setLoading(false)
+    
+                console.log('terminando la validacion')
+            }, 3000)
+        }
+
+        console.log('terminando el efecto')
+    }, [loading])
 
     return (
         <div>
@@ -13,9 +30,13 @@ function UseState({ name }) {
                 <p>Error: El codigo es incorrecto !</p>
             ) }
 
+            { loading && (
+                <p>Cargando...</p>
+            ) }
+
             <input placeholder="codigo de seguridad" />
             <button
-                onClick={() => setError(!error)}
+                onClick={() => setLoading(true)}
             >Comprobar</button>
         </div>
     );
