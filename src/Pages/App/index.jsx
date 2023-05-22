98acaca675ca8482189344cb3b61a8d306ac148a
ed oliveros <edgarolimoy98@gmail.com>
2023-05-22 15:36:17 -0500

    feat: :art: React-router-dom es genial!!

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,22 +1,31 @@
-import Home from '../Home'
-import MyAcount from '../MyAcount'
-import MyOrder from '../MyOrder'
-import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
-import Signin from '../Signin'
-import './App.css'
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import Home from '../Home';
+import MyAcount from '../MyAcount';
+import MyOrder from '../MyOrder';
+import MyOrders from '../MyOrders';
+import NotFound from '../NotFound';
+import Signin from '../Signin';
+import './App.css';
 
-function App() {
-  return (
-    <div className='bg-red-100'>
-      <Home />
-      <MyAcount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <Signin />
-    </div>
-  )
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAcount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sign-in', element: <Signin /> },
+  ])
+
+  return routes
 }
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+};
 
 export default App
